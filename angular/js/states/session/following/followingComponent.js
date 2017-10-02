6fa0ab07f92a63809e1d6523024e13825d975fe4
unknown <Greg.Marshall.Hill@gmail.com>
2017-10-02 13:05:10 -0500

    Refactored file structure

--- angular/js/states/session/following/followingComponent.js
+++
@@ -1,6 +0,0 @@
-angular.module('twitterClone').componenet('follwingComponent',{
-    
-    templateUrl : './js/states/session/follwing/follwingTemplate.html',
-        controller : './js/states/session/follwing/follwingController.js'
-    
-    })
