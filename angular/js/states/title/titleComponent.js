cdf0768457b6e285a0ad80c65a2ae1a4b997fada
ThanujaReddy05 <thanu.reddy1985@gmail.com>
2017-10-02 13:47:28 -0500

    df

--- angular/js/states/title/titleComponent.js
+++ angular/js/states/title/titleComponent.js
@@ -1,4 +1,4 @@
-angular.module('twitterClone').componenet('titleComponent', {
+angular.module('twitterClone').component('titleComponent', {
 
     templateUrl: './js/states/title/titleTemplate.html'
 
